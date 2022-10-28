bf80e9628d05702f97a6c5bd32d7bbd977e8f0d9
Ameya <ameyasawant5@gmail.com>
2022-10-28 22:17:40 +0530

    Changes related to enabling transaction in the database config

--- data-access/src/main/java/com/data/config/DatabaseConfig.java
+++ data-access/src/main/java/com/data/config/DatabaseConfig.java
@@ -14,11 +14,14 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @Log4j2
+@EnableTransactionManagement
 public class DatabaseConfig {
 
     @Autowired
